1edc976487dcbf0fb9693bc50ecd79dd81b76d9e
Beth Stranz <bethstranz@hotmail.com>
2015-03-19 21:59:42 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LodgeOnRails::Application.config.secret_token = 'a021175c02c912e882788f6333ee5bd32bb855c2042844fd69b23de70b63470a04051fbf0824812a2dae3023ee4da2fcb83bf0e933c5e7fcd2ba84b1dd9e2b77'
